fb81438d0ba890a3cbf25ce8f41f9db9274bc022
Kunal-ahuja-88 <kahujamp4@gmail.com>
2024-07-21 02:52:22 +0530

    sending ipfs hash from client to smart contract

--- client/src/components/uploadImage.jsx
+++ client/src/components/uploadImage.jsx
@@ -1,14 +1,35 @@
 import axios from "axios"
 import { useState } from "react"
+import { useWeb3Context } from "../contexts/useWeb3context"
+import toast from "react-hot-toast"
+
 const UploadImage = () => {
     const [file,setFile] = useState(null)
-    const handleImageUpload = async() => {
-        const formData = new FormData();
-        formData.append("file",file) 
+    const {web3State} = useWeb3Context()
+    const {selectedAccount , contractInstance} = web3State;
 
-        const url = `http://localhost:3000/api/uploadImage`
-        const res = await axios.post(url,formData);
-        console.log(res.data);
+    const uploadImageHash = async(ipfsHash) => {
+        const tsc = await contractInstance.uploadFile(selectedAccount,ipfsHash)
+        console.log(tsc)
+    }
+
+    const handleImageUpload = async() => {
+
+        try {
+            const formData = new FormData();
+            formData.append("file",file) 
+    
+            const url = `http://localhost:3000/api/uploadImage`
+            const res = await axios.post(url,formData);
+            toast.success("image uploaded")
+            await uploadImageHash(res.data.ipfsHash);
+
+        } catch (error) {
+            console.log(error)
+            toast.error("image upload failed")
+        }
+       
+       
     }
   console.log(file)
     return (
